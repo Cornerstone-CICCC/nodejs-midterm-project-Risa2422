76bbf7012fea15b2c5c65e938b16be5a2545a936
Risa Yamamoto <yamari2422@gmail.com>
2024-11-16 13:58:25 -0800

    add styling to the Home page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,9 +2,15 @@ import RecipeCardList from "../components/custom/RecipeCardList";
 
 export default function Home() {
   return (
-    <div className="p-20">
+    <div className="p-16">
       <main className="flex flex-col row-start-2 items-center">
         {/* <Search /> */}
+        <div className="flex flex-col items-center gap-1 mb-4">
+          <h1 className="text-3xl	">Taste the World, One Recipe at a Time</h1>
+          <p className="text-md">
+            Get inspired and try something new in your kitchen every day.
+          </p>
+        </div>
         <RecipeCardList isMypage={false} userId={"0"} />
       </main>
     </div>
--- frontend/components/custom/RecipeCard.tsx
+++ frontend/components/custom/RecipeCard.tsx
@@ -10,7 +10,7 @@ type Props = {
 const RecipeCard = (props: Props) => {
   return (
     <Link href={`/recipe/${props.recipe.id}`}>
-      <div className="my-8 w-52 rounded shadow-lg shadow-gray-200 dark:shadow-gray-900 bg-white dark:bg-gray-800 duration-300 hover:-translate-y-1">
+      <div className="my-3 w-[220px] rounded  bg-white dark:bg-gray-800 duration-300 hover:-translate-y-1 relative">
         <CldImage
           src={props.recipe.image}
           width="500"
@@ -19,12 +19,38 @@ const RecipeCard = (props: Props) => {
             type: "auto",
             source: true,
           }}
-          alt=""
+          alt="recipe image"
         />
-        <div className="p-4">
-          <h2 className="text-xl font-semibold text-gray-800">
+        <div className="py-3 px-2 flex flex-col">
+          <h2 className="text-lg font-semibold text-gray-800 overflow-auto">
             {props.recipe.title}
           </h2>
+          <div className="flex gap-3">
+            <div className="flex items-center gap-0.5">
+              <div className="w-4">
+                <img src="/location.png" alt="location" />
+              </div>
+              <p className="text-sm">{props.recipe.cuisineType}</p>
+            </div>
+            <div className="flex items-center gap-0.5">
+              <div className="w-4">
+                <img src="/timer.png" alt="cooking time" />
+              </div>
+              <p className="text-sm">{props.recipe.cookingTime}mins</p>
+            </div>
+          </div>
+        </div>
+        <div
+          className={`absolute top-2 right-2 px-2 py-0.5 rounded-md text-sm text-white 
+            ${
+              props.recipe.difficulty === "Easy"
+                ? "bg-[rgba(53,114,111,0.6)]"
+                : props.recipe.difficulty === "Medium"
+                ? "bg-[rgba(202,161,90,0.6)]"
+                : "bg-[rgba(144,65,58,0.6)]"
+            }`}
+        >
+          {props.recipe.difficulty}
         </div>
       </div>
     </Link>
