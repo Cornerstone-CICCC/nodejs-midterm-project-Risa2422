7388d119f9ab9f2f0e1009c71ecc7193416c0fd7
Risa Yamamoto <yamari2422@gmail.com>
2024-11-16 11:09:08 -0800

    add sample data

--- backend/src/models/recipe.model.ts
+++ backend/src/models/recipe.model.ts
@@ -7,11 +7,12 @@ class RecipeModel {
     {
       id: "1",
       userId: "1",
-      title: "Takoyaki",
-      cuisineType: "Japanese",
-      cookingTime: 10,
+      title: "Pho",
+      cuisineType: "Vietnamese",
+      cookingTime: 20,
       difficulty: "easy",
-      image: "",
+      image:
+        "https://res.cloudinary.com/do9wpvadq/image/upload/v1731754888/uploads/xfh6n2r4ammduqaktptq.jpg",
     },
     {
       id: "2",
@@ -20,25 +21,69 @@ class RecipeModel {
       cuisineType: "Japanese",
       cookingTime: 10,
       difficulty: "easy",
-      image: "",
+      image:
+        "https://res.cloudinary.com/do9wpvadq/image/upload/v1731782725/TSU_mayomayookonomi_TP_V4_ntspye.jpg",
     },
     {
       id: "3",
-      userId: "90393cd5-ab1f-40fc-be7f-ca2dfaccb438",
+      userId: "19988b16-58b9-4b2c-9147-3a84307982ae",
       title: "Bibimbap",
       cuisineType: "Korean",
       cookingTime: 10,
       difficulty: "easy",
-      image: "",
+      image:
+        "https://res.cloudinary.com/do9wpvadq/image/upload/v1731782785/pexels-becerragoveaphoto-5773960_ecdfsd.jpg",
     },
     {
       id: "4",
-      userId: "90393cd5-ab1f-40fc-be7f-ca2dfaccb438",
+      userId: "19988b16-58b9-4b2c-9147-3a84307982ae",
       title: "Toppoki",
       cuisineType: "Korean",
       cookingTime: 10,
       difficulty: "easy",
-      image: "",
+      image:
+        "https://res.cloudinary.com/do9wpvadq/image/upload/v1731782912/pexels-pip-pip-1842307-12963392_fsj5il.jpg",
+    },
+    {
+      id: "5",
+      userId: "19988b16-58b9-4b2c-9147-3a84307982ae",
+      title: "Tacos",
+      cuisineType: "Mexican",
+      cookingTime: 20,
+      difficulty: "easy",
+      image:
+        "https://res.cloudinary.com/do9wpvadq/image/upload/v1731783192/pexels-cristian-rojas-7613568_nc6q98.jpg",
+    },
+    {
+      id: "6",
+      userId: "90393cd5-ab1f-40fc-be7f-ca2dfaccb438",
+      title: "Bubble Tea",
+      cuisineType: "Taiwanese",
+      cookingTime: 20,
+      difficulty: "easy",
+      image:
+        "https://res.cloudinary.com/do9wpvadq/image/upload/v1731783567/pexels-bam-awey-348707-4013151_pepa84.jpg",
+    },
+    {
+      id: "7",
+      userId: "90393cd5-ab1f-40fc-be7f-ca2dfaccb438",
+      title: "Adobo",
+      cuisineType: "Filipino",
+      cookingTime: 20,
+      difficulty: "easy",
+      image:
+        "https://res.cloudinary.com/do9wpvadq/image/upload/v1731783740/pexels-fox-58267-6525933_lufl5d.jpg",
+    },
+
+    {
+      id: "8",
+      userId: "90393cd5-ab1f-40fc-be7f-ca2dfaccb438",
+      title: "Tamales",
+      cuisineType: "Costa Rican",
+      cookingTime: 20,
+      difficulty: "easy",
+      image:
+        "https://res.cloudinary.com/do9wpvadq/image/upload/v1731783861/pexels-gonzalogfg-14179987_g3ardf.jpg",
     },
   ];
 
